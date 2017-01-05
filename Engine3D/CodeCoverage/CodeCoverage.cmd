a9c540d4bf8f1c087615df46f91be9e32e5484cd
Gavin Murrison <github@ninemail.net>
2017-01-05 23:32:22 +0000

    Update CodeCoverage.cmd

--- Engine3D/CodeCoverage/CodeCoverage.cmd
+++ Engine3D/CodeCoverage/CodeCoverage.cmd
@@ -11,7 +11,7 @@ rem cd ..
 rem cd ..
 
 rem The -threshold options prevents this taking ages...
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" -targetargs:"/noresults /noisolation /testcontainer:"".\Engine3D\Engine3D-Tests\bin\Release\Engine3D-Tests.dll"" -threshold:10 -filter:"+[Engine3D*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\Engine3D.Coverage.xml
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" -targetargs:"/noresults /noisolation /testcontainer:"".\Engine3D\Engine3D-Tests\bin\Release\Engine3D-Tests.dll" -threshold:10 -filter:"+[Engine3D*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\Engine3D.Coverage.xml
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
