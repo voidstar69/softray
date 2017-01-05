0f6d49efd5d7a54ccd83861b378b4c18984287d2
voidstar69 <github@ninemail.net>
2017-01-05 23:01:36 +0000

    Added OpenCover for code coverage via CI and codecov.io website (experimental)

---
+++ Engine3D/CodeCoverage/CodeCoverage.cmd
@@ -0,0 +1,20 @@
+@echo off
+cd Engine3D\CodeCoverage
+
+nuget restore packages.config -PackagesDirectory .
+
+cd ..\Engine3D-Tests
+
+dotnet restore
+
+cd ..
+cd ..
+
+rem The -threshold options prevents this taking ages...
+Engine3D\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test Engine3D\Engine3D-Tests -c Release -f net451" -threshold:10 -register:user -filter:"+[Engine3D*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\Engine3D.Coverage.xml
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
+pip install codecov
+codecov -f "Engine3D.Coverage.xml"
