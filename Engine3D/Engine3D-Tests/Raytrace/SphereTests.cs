d8100011c296ba704f86a2706bb30e334c90171a
voidstar69 <github@ninemail.net>
2018-03-18 18:06:58 +0000

    Created tests for Sphere class

---
+++ Engine3D/Engine3D-Tests/Raytrace/SphereTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Engine3D;
+using Engine3D.Raytrace;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Engine3D_Tests.Raytrace
+{
+    [TestClass]
+    public class SphereTests
+    {
+        [TestMethod, ExpectedException(typeof(NotImplementedException))]
+        public void IntersectLineSegmentTest()
+        {
+            var sphere = new Sphere(Vector.Zero, 1.0);
+            sphere.IntersectLineSegment(Vector.Zero, Vector.Zero);
+        }
+
+        [TestMethod]
+        public void ContainsPointTest()
+        {
+            var sphere = new Sphere(Vector.Zero, 1.0);
+            Assert.IsTrue(sphere.ContainsPoint(Vector.Zero));
+            Assert.IsTrue(sphere.ContainsPoint(Vector.Right * 0.999));
+            Assert.IsFalse(sphere.ContainsPoint(Vector.Right));
+            Assert.IsFalse(sphere.ContainsPoint(Vector.Right * 1.0001));
+            Assert.IsFalse(sphere.ContainsPoint(Vector.Right * 2));
+        }
+    }
+}
