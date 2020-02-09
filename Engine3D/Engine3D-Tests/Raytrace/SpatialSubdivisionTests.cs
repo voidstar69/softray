0af1dcc3bdba8da11163ea7898395cd2c5c649c3
voidstar69 <voidstar69@github.com>
2020-02-09 19:46:41 +0000

    Adjust margin on one test

--- Engine3D/Engine3D-Tests/Raytrace/SpatialSubdivisionTests.cs
+++ Engine3D/Engine3D-Tests/Raytrace/SpatialSubdivisionTests.cs
@@ -202,7 +202,7 @@ namespace Engine3D_Tests
 #elif APPVEYOR_PERFORMANCE_MARGINS
             // AppVeyor build server
             const double minMillionRaysPerSec = 60.0;
-            const double maxMillionRaysPerSec = 72.0;
+            const double maxMillionRaysPerSec = 82.0;
 #else
             // my laptop in High Performance mode
             const double minMillionRaysPerSec = 35.0;
